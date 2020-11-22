f4f93202124130e1417577fe9334ae432964aa44
Iván Rodríguez Núñez <rbfsoft@gmail.com>
2020-11-22 12:14:41 +0100

    route Element

---
+++ InventoryManager.WebMVC/Controllers/ElementController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManager.WebMVC.Controllers
+{
+    public class ElementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
