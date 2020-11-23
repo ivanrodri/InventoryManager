91f1b0c80a2e17f8b38d6f1a3095de0c0c88eb4c
Iván Rodríguez Núñez <rbfsoft@gmail.com>
2020-11-23 23:34:12 +0100

    Element Create

---
+++ InventoryManager.Application/Elements/ElementService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InventoryManager.Domain.Elements;
+
+namespace InventoryManager.Application.Element
+{
+    public class ElementService : IElementService
+    {
+        //readonly IRepository<Product> productRepository;
+        //readonly IRepository<ProductCode> productCodeRepository;
+        //readonly IUnitOfWork unitOfWork;
+
+
+        public ElementDto Add(ElementDto element)
+        {
+            //ProductCode productCode =
+            //    this.productCodeRepository.FindById(productDto.ProductCodeId);
+
+            //if (productCode == null)
+            //    throw new Exception("Product Code Is Not Valid");
+
+          Element element = Element
+
+            //this.productRepository.Add(product);
+
+            return AutoMapper.Mapper.Map<Element, ElementDto>(element);
+        }
+
+        public void Extract(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ElementDto Get(Guid elementId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InventoryManager.Domain/Elements/Element.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InventoryManager.Domain.Elements
+{
+     public class Element
+    {
+        public Guid Id { get; protected set; }
+
+        public string Name { get; protected set; }
+
+        public DateTime ExpirationDate { get; protected set; }
+
+        public DateTime EntryDate { get; protected set; }
+
+        public string Type { get; protected set; }
+
+
+        public static Element Create(string name, DateTime expirationDate, DateTime entryDate, string type )
+        {
+            return Create(Guid.NewGuid(), name, expirationDate, entryDate, type);
+        }
+
+        public static Element Create(Guid id, string name, DateTime expirationDate, DateTime entryDate, string type)
+        {
+            Element product = new Element()
+            {
+                Id = id,
+                Name = name,
+                ExpirationDate = expirationDate,
+                EntryDate = DateTime.Now,
+                Type = type,
+
+            };
+
+
+            return product;
+        }
+
+
+
+
+    }
+}
