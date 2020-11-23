b25f1aeca59147bc1d6fb222c5c89cfa1ceca7c3
Iván Rodríguez Núñez <rbfsoft@gmail.com>
2020-11-24 00:21:20 +0100

    AddAutoMapper Api

--- InventoryManager.API/Startup.cs
+++ InventoryManager.API/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace InventoryManager.API
 {
@@ -26,6 +27,7 @@ namespace InventoryManager.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
