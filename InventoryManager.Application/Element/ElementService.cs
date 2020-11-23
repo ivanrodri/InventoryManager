e5a811995707d8e8543539efd28801cdd30af5c9
Iván Rodríguez Núñez <rbfsoft@gmail.com>
2020-11-23 23:04:23 +0100

    ElementService

---
+++ InventoryManager.Application/Element/ElementService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManager.Application.Element
+{
+    public class ElementService : IElementService
+    {
+        public ElementDto Add(ElementDto element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Extract(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ElementDto Get(Guid elementId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
