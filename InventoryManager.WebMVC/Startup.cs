2a3da94a544148a48eab382328dc6aad097f13bc
Iván Rodríguez Núñez <rbfsoft@gmail.com>
2020-11-26 19:38:41 +0100

    Services on MVC

--- InventoryManager.WebMVC/Startup.cs
+++ InventoryManager.WebMVC/Startup.cs
@@ -1,3 +1,5 @@
+using InventoryManager.Application.Elements;
+using InventoryManager.Infraestructura;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,11 @@ namespace InventoryManager.WebMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddTransient<IElementService, ElementService>();
+
+            services.AddScoped(typeof(IRepository<>), typeof(MemoryRepository<>));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
